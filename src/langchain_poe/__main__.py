9cf0bcbe06207c81e2a0fbfaa6bedd52e8fc310a
Aja Sibley <aja@thinkum.local>
2023-05-31 20:45:19 +0000

    updated

--- src/langchain_poe/__main__.py
+++ src/langchain_poe/__main__.py
@@ -5,4 +5,4 @@ from fastapi_poe import run
 from .poe import LangChainCatBot
 
 if __name__ == "__main__":
-    run(LangChainCatBot("os.environ["OPENAI_API_KEY"]"))
+    run(LangChainCatBot(os.environ["OPENAI_API_KEY"]))
