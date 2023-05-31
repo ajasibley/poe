74276fc40be03a09a352a35404d2b5588df0b3d3
Aja Sibley <aja@thinkum.local>
2023-05-31 22:47:00 +0000

    updated

--- src/langchain_poe/__main__.py
+++ src/langchain_poe/__main__.py
@@ -1,13 +1,8 @@
 import os
-import importlib
 
 from fastapi_poe import run
 
-import poe
-from poe import LangChainCatBot
+from .poe import LangChainCatBot
 
 if __name__ == "__main__":
-    # Reload the 'poe' module to apply the changes
-    importlib.reload(poe)
-
     run(LangChainCatBot(os.environ["OPENAI_API_KEY"]))
